43b4f8cd841eb6b29ae5552bd76ac10225e082f9
jyoti92173 <jyoti53913@gmail.com>
2025-01-01 21:57:06 +0530

    Updated Solution

--- Day9/SubTreeOfAnother.java
+++ Day9/SubTreeOfAnother.java
@@ -22,13 +22,16 @@ public class TreeNode {
 }
 class SubTreeOfAnother{ 
     public boolean isIdentical (TreeNode root, TreeNode subRoot){
-        if(root== null && subRoot == null){
+         if(root == null && subRoot == null){
             return true;
         }
-        if(root == null || subRoot == null){
+        if(root == null || subRoot== null){
             return false;
         }
+        if(root.val == subRoot.val){
         return isIdentical(root.left, subRoot.left) && isIdentical(root.right, subRoot.right);
+        }
+        return false;
     } 
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         if(subRoot == null){
